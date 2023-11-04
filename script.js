3482e1a4f6040c49d769cd7921eb9ac7a2178a55
joshuadecano <70615417+joshuadecano@users.noreply.github.com>
2023-11-03 18:23:02 -0700

    created the structure of the page

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-const container = document.querySelector('.container');
+const sidebar = document.querySelector('.sidebar');
+const logobg = document.querySelector('.logobg');
+const credit = document.querySelector('.credit');
+const sidebg = document.querySelector('#side-bg');
+const logobox = document.querySelector('.logobox');
+
+sidebar.style.width = sidebg.style.width;
+logobg.style.width = sidebg.style.width;
+credit.style.width = sidebg.style.width;
+logobox.style.width = sidebg.style.width;
