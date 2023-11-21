6f1c7b58661560b20ddd7291c8e04f50202e6b8a
joshuadecano <70615417+joshuadecano@users.noreply.github.com>
2023-11-21 13:27:38 -0800

    Project complete! See notes... In this commit I made it so the passwords must match before you can submit. I could do more like add @media only screen for screens smaller than 600px, but I'll save that for another time/project!

--- script.js
+++ script.js
@@ -1,19 +1,17 @@
-const centerBox = document.querySelector('.centerBox');
-const forms = document.querySelector('.formc');
-
-//const pass = document.querySelector('input[name="password"]')
-//const passConf = document.querySelector('input[name="confPass"]')
-const error = document.querySelectorAll('.error')
-
+const pass = document.getElementById('pass');
+const passConf = document.getElementById('passConf');
+const error = document.querySelectorAll('.error');
+const buttons = document.getElementById('button');
 
 function checkPassword() {
     const test = document.querySelector('#confPass');
     if (document.getElementById('pass').value != 
     document.getElementById('confPass').value) {
-        test.classList.add("error")
+        test.classList.add("error");
+        buttons.disabled = true;
     }
     else {
-        //document.getElementById('confPass').style.borderColor = 'red';
-        test.classList.remove("error")
+        test.classList.remove("error");
+        buttons.disabled = false;
     }
 }
